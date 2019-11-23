ecc329110b75dfc046b3d865ac6b069c406dd3ea
Daniel Rodriguez del Villar Trimarchi <daniel@dnd1.com>
2019-11-23 05:24:17 -0400

    Added images and avatar

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
+import github from '../img/github-icon.svg'
+import twitter from '../img/twitter-icon.png'
 
 export const HTMLContent = ({ content, className }) => (
   <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
 )
 
 const Content = ({ content, className }) => (
-  <div className={className}>{content}</div>
+  <div className={className}>
+    {content}
+  </div>
 )
 
 Content.propTypes = {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link, StaticQuery, graphql } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import twitter from '../img/twitter-icon.png'
+import youtube from '../img/youtube-icon.png'
 
 const Navbar = () => (
   <StaticQuery
@@ -22,11 +23,19 @@ const Navbar = () => (
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item">
-              <h2 class="title is-2">DANIELRVT</h2> 
+              <img
+                src="https://gravatar.com/userimage/33319223/785e4839979cd343ef431e138e2c8ba2?size=200"
+                style={{
+                  borderRadius: '50%',
+                  maxHeight: '6rem',
+                  margin: '.25rem 2rem 0 0',
+                }}
+              />
+              <h2 class="title is-2">DANIELRVT</h2>
             </Link>
           </div>
           <div className="navbar-start">
-            {data.allWordpressPage.edges.map(edge => (
+            {/* {data.allWordpressPage.edges.map(edge => (
               <Link
                 className="navbar-item"
                 to={edge.node.slug}
@@ -34,19 +43,43 @@ const Navbar = () => (
               >
                 {edge.node.title}
               </Link>
-            ))}
+            ))} */}
           </div>
           <div className="navbar-end">
-            <a
-              className="navbar-item"
-              href="https://github.com/GatsbyCentral/gatsby-starter-wordpress"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="icon">
-                <img src={github} alt="Github" />
-              </span>
-            </a>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+              <a
+                className="navbar-item is-block"
+                href="https://github.com/danielrvt"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="icon">
+                  <img src={github} alt="Github" />
+                </span>
+              </a>
+
+              <a
+                className="navbar-item is-block"
+                href="https://twitter.com/danielrvt"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="icon">
+                  <img src={twitter} alt="Twitter" />
+                </span>
+              </a>
+
+              <a
+                className="navbar-item is-block"
+                href="https://www.youtube.com/channel/UC_1FcCZgSWNRiqossWAqIog"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="icon">
+                  <img src={youtube} alt="Youtube" />
+                </span>
+              </a>
+            </div>
           </div>
         </div>
       </nav>
